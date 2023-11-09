3060df1ed33d12c6552e222a590b2d52d349c0cc
trBizhan <145677592+trBizhan@users.noreply.github.com>
2023-11-09 11:23:51 +0000

    Fix: index file loop for user input, still need polishing

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,42 @@ console.log(
   `The rover starting coordinate: x = ${c.x} y = ${c.y} orientation = ${c.o}`
 );
 
+function replDemo() {
+  return new Promise(function (resolve, reject) {
+    let rl = readline.createInterface(process.stdin, process.stdout);
+    rl.setPrompt("enter a command for Rover: ");
+    rl.prompt();
+    rl.on("line", function (line) {
+      if (line === "exit" || line === "quit" || line == "q") {
+        rl.close();
+        return; // exit
+      }
+
+      if (line === "help" || line === "?") {
+        console.log(`permitted command a combination of: L|R|M\n`);
+      } else {
+        console.log(commandHandler(line));
+      }
+      rl.prompt();
+    }).on("close", function () {
+      console.log("bye");
+      resolve(42); // this is the final result of the function
+    });
+  });
+}
+
+async function run() {
+  try {
+    let replResult = await replDemo();
+    console.log("repl result:", replResult);
+  } catch (e) {
+    console.log("failed:", e);
+  }
+}
+
+run();
+
+/*
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -24,3 +60,4 @@ rl.question("Entr a command for Rover or q for quit : ", (inst) => {
     rl.close();
   }
 });
+*/
